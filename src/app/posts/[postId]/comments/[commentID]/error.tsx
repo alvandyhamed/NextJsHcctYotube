432eb9c522e441636856bdadaea9913536b73807
Hamed <alvandyhamed@gmail.com>
2024-01-12 22:28:06 +0330

    Feat(section24):error refrsh

--- src/app/posts/[postId]/comments/[commentID]/error.tsx
+++ src/app/posts/[postId]/comments/[commentID]/error.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React from "react";
 
-function error() {
+function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
   return (
     <div>
-      <h1>We have Comments Error</h1>
+      <h1>
+        {error.message} <button onClick={reset}>Try again</button>
+      </h1>
     </div>
   );
 }
 
-export default error;
+export default ErrorBoundary;
